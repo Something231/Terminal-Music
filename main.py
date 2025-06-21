ec4f284758f1252922f06be1b98ac5d3971b4160
Something231 <119870385+Something231@users.noreply.github.com>
2025-06-21 23:46:05 +0800

    working well, search music and choose

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ ydl_opts = {
 def yt_search(quary):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(f"ytsearch5:{quary}", download=False)
+    result = {}
     for video in info.get("entries"):
-        logging.debug(str(video.get("title"))+f"https://www.youtube.com/watch?v={video['id']}")
+        result.update({str(video.get("title")): f"https://www.youtube.com/watch?v={str(video['id'])}"})
+    return result
 
 def stream_youtube(url): 
     #function partially by ai, at least for now
@@ -35,39 +37,54 @@ def stream_youtube(url):
 
 def main(stdscr: curses.window):
     curses.start_color()
-    curses.curs_set(0)
     curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_RED)
 
     stdscr.nodelay(True)
     stdscr.keypad(True)
 
+    inputbox = ""
     selectedrow = 0
-    choices = {"rick": "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "other": "https://www.youtube.com/watch?v=XkFz_hi2tWY"}
+    choices = {}
 
     while True:
         stdscr.clear()
-        stdscr.addstr(0, 0, "Choose something to play below:")
+        stdscr.addstr(0, 0, "Type and press enter to search below:", curses.A_BOLD)
 
         for i, row in enumerate(choices.keys()):
-            if i == selectedrow:
-                stdscr.addstr(i+1, 0, row, curses.color_pair(2))
+            if i+1 == selectedrow:
+                stdscr.addstr(i+2, 0, row, curses.color_pair(2))
             else:
-                stdscr.addstr(i+1, 0, row, curses.color_pair(1))
+                stdscr.addstr(i+2, 0, row, curses.color_pair(1))
 
-        stdscr.refresh()
         key = stdscr.getch()
-
         if key == curses.KEY_UP and selectedrow != 0:
             selectedrow -= 1
-        elif key == curses.KEY_DOWN and selectedrow != len(choices)-1:
+        elif key == curses.KEY_DOWN and selectedrow != len(choices):
             selectedrow += 1
-        elif key == 10 or key == 13:
-            if player != None and player.poll() is None:
-                player.terminate()
-            logging.debug(list(choices.values())[selectedrow])
-            stream_youtube(list(choices.values())[selectedrow])
+
+        if selectedrow == 0:
+            curses.curs_set(1)
+            if key == curses.KEY_BACKSPACE:
+                inputbox = inputbox[:-1]
+            elif key in (10, 13, curses.KEY_ENTER):
+                logging.debug(inputbox)
+                choices = yt_search(inputbox)
+            elif 32 <= key <= 126:
+                char = chr(key)
+                inputbox = inputbox + str(char)
+            stdscr.move(1, len(inputbox)+1)
+
+        else:
+            curses.curs_set(0)
+            if key == 10 or key == 13:
+                if player != None and player.poll() is None:
+                    player.terminate()
+                logging.debug(list(choices.values())[selectedrow-1])
+                stream_youtube(list(choices.values())[selectedrow-1])
         
+        stdscr.addstr(1, 0, inputbox)
+        stdscr.refresh()
         time.sleep(0.03)
 
 if __name__ == "__main__":
